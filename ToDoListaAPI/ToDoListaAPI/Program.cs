d764c3db2dd4d3e4b1d9c2a1b209d397d552df2f
VedranDzanko <132369519+VedranDzanko@users.noreply.github.com>
2023-10-08 21:16:19 +0200

    update

--- ToDoListaAPI/ToDoListaAPI/Program.cs
+++ ToDoListaAPI/ToDoListaAPI/Program.cs
@@ -47,20 +47,24 @@ builder.Services.AddDbContext<ToDoContext>(o =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger(opcije =>
 {
-    app.UseSwagger(opcije =>
-    {
-        opcije.SerializeAsV2 = true;
-    });
-    app.UseSwaggerUI(opcije =>
-    {
-        opcije.ConfigObject.
-        AdditionalItems.Add("requestSnippetsEnabled", true);
-    });
-}
+    opcije.SerializeAsV2 = true;
+});
+app.UseSwaggerUI(opcije =>
+{
+    opcije.ConfigObject.
+    AdditionalItems.Add("requestSnippetsEnabled", true);
+});
+//}
 
 app.UseHttpsRedirection();
 app.MapControllers();
 app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+app.UseDefaultFiles();
+app.UseDeveloperExceptionPage();
+app.MapFallbackToFile("index.html");
 app.Run();
